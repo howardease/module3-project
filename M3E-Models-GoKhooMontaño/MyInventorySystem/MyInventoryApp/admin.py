a0da1217d7951bc76fc8fddc5f7f7427d6e690d0
howardease <hayden.khoo@student.ateneo.edu>
2025-02-27 06:04:51 +0800

    Update admin.py

--- M3E-Models-GoKhooMontaño/MyInventorySystem/MyInventoryApp/admin.py
+++ M3E-Models-GoKhooMontaño/MyInventorySystem/MyInventoryApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Supplier
+admin.site.register(Supplier)
+
+from .models import WaterBottle
+admin.site.register(WaterBottle)
